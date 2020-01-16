9885c7e3fe8f0b118eae06700c7cfdf6cc948586
SERLUK <luca_serj87@mail.ru>
2020-01-16 19:15:33 +0300

    add line

--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -12,3 +12,35 @@ def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
 	"""
 	print(g, start_node)
 	return list(g.nodes)
+
+def dfs_find( graph,src,dst,visited):
+	visited[src] = True
+	if src == dst: #src -откуда мы начинаем dst -куда мы хотим доити
+		return True
+
+
+	for node in graph.adj[src]:
+		if not visited[node]:
+			if dfs_find(graph,node,dst,visited):
+				return True
+	return False
+
+
+
+if __name__=="__main__":
+	graph = nx.Graph()
+	graph.add_nodes_from("ABCDEFG")
+	graph.add_edges_from(
+		[("A", "B"),
+		 ("A", "C"),
+		 ("B", "D"),
+		 ("B", "E"),
+		 ("C", "F"),
+		 ("E", "G"),]
+	)
+	graph.add_node("Z")
+	src = "A"
+	dst = "Z"
+
+	visited = {node: False for node in graph.nodes()}
+	print(dfs_find(graph,src,dst,visited))
